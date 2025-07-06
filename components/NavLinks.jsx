810f3c17b45d400291f0bd5cd8d71afc9a6f86d7
harymarc08 <marchary08@gmail.com>
2025-07-06 15:19:26 +0300

    service deleted

--- components/NavLinks.jsx
+++ components/NavLinks.jsx
@@ -8,7 +8,6 @@ import clsx from 'clsx';
 const links = [
   { name: "home", path: "/" },
   { name: "about", path: "/about" },
-  { name: "services", path: "/services" },
   { name: "work", path: "/work" },
   { name: "contact", path: "/contact" },
 ];
